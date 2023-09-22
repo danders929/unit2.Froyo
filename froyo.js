233972f691ce16b3c792c0087ee67850cc48e807
Dan <sandersda@gmail.com>
2023-09-22 09:58:00 -0500

    final changes

--- froyo.js
+++ froyo.js
@@ -2,15 +2,19 @@
 const order = prompt(
   "Please enter a list of froyo flavors separated by commas.",
   "vanilla,vanilla,strawberry,chocolate,coffee");
+
 // Splits user input into an array using comma separated values
 const usrFlavorArray = order.split(",");
 const froyo = {}
+
 // adds each flavor into the froyo object
-for (flavor in usrFlavorArray){
-  let flavName = (usrFlavorArray[flavor]);
-  //If flavor exists object is increased by 1, if it doesn't value is set to 1
-  froyo[flavName] = froyo[flavName] +1 || 1; 
+function count(flavors){
+  for (const flavor of usrFlavorArray){
+    // If flavor exists object is increased by 1, if it doesn't value is set to 1
+    froyo[flavor] = froyo[flavor] +1 || 1; 
+  }
 }
 
-
-
+// Executes function using the user input array and displays a table showing the count.
+count(usrFlavorArray);
+console.table(froyo);
